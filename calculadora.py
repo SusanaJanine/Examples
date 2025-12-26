6e0e128c2610d2541ced7a06b44dfa866a9e72bd
SusanaJanine <susanajaninevelezflores@gmail.com>
2025-12-26 19:13:15 +0000

    confirm

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+# funciones basicas de la calculadora
+def sumar(x, y):
+    return x + y
+
+def restar(x, y):
+    return x - y
+
+def multiplicar(x, y)
+    return x * y
+
+def dividir (x, y):
+    if y == 0:
+        return "no se puede divir entre 0"
+    else:
+        return x / y
+    
---
+++ script.py
@@ -0,0 +1,20 @@
+def menu():
+    print('CALCULADORA SIMPLE')
+    print('1, Sumar')
+    print('2, Restar')
+    print('3, Multiplicar')
+    print('4, Dividir')
+    print('5, Salir')
+
+def calculadora():
+    while True:
+        menu()
+        opcion = input('ingrese una opcion:')
+
+        if opcion = '5':
+            print('Gracias por usar la calculadora')
+            break
+
+        try: 
+
+calculadora()
