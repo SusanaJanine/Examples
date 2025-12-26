602fcedc76f92498c8400a59ad0eade68802a2c4
SusanaJanine <susanajaninevelezflores@gmail.com>
2025-12-26 22:44:55 +0000

    confirm

--- script.py
+++ script.py
@@ -11,10 +11,29 @@ def calculadora():
         menu()
         opcion = input('ingrese una opcion:')
 
-        if opcion = '5':
+        if opcion == '5':
             print('Gracias por usar la calculadora')
             break
 
-        try: 
+        try:
+            num1 = float(input('Ingrese el primer número: '))
+            num2 = float(input('Ingrese el segundo número: '))
+        except ValueError:
+            print('Ingrese números válidos')
+            continue
+
+        if opcion == '1':
+            print(f'El resultado de la suma es: {num1 + num2}')
+        elif opcion == '2':
+            print(f'El resultado de la resta es: {num1 - num2}')
+        elif opcion == '3':
+            print(f'El resultado de la multiplicación es: {num1 * num2}')
+        elif opcion == '4':
+            if num2 != 0:
+                print(f'El resultado de la división es: {num1 / num2}')
+            else:
+                print('Error: No se puede dividir por cero')
+        else:
+            print('Opción no válida, por favor intente de nuevo')
 
 calculadora()
